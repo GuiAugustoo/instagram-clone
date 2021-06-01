fc0637d571eb927e78f068fc8bc6a9111101f137
GuiAugusto <guicruz539@gmail.com>
2021-06-01 09:36:13 -0300

    Update firebase.js

--- instagram-clone-curso/src/firebase.js
+++ instagram-clone-curso/src/firebase.js
@@ -1,13 +1,13 @@
 import firebase from 'firebase';
 
 const fireBaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyBjOUi0gwUb52bJ7A2zT1ugI6MHZLm6AmU",
-  authDomain: "instagram-clone-curso-a8fd4.firebaseapp.com",
-  projectId: "instagram-clone-curso-a8fd4",
-  storageBucket: "instagram-clone-curso-a8fd4.appspot.com",
-  messagingSenderId: "201127479801",
-  appId: "1:201127479801:web:7f50304abe1259c9711528",
-  measurementId: "G-8ZEKYT5CC5"
+  apiKey: "Sua apiKey",
+  authDomain: "Seu authDomain",
+  projectId: "Seu projectId",
+  storageBucket: "Seu storageBucket",
+  messagingSenderId: "Seu messagingSenderId",
+  appId: "Seu appId",
+  measurementId: "Seu measurementId"
 });
 
 const db = firebase.firestore();
